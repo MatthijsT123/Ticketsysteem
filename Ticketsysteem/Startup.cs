6851c60ad4e966ab05af4c3f7d1cbc95965a8273
MatthijsT123 <matthijstuijl@hotmail.com>
2019-12-04 12:31:27 +0100

    Add project files.

---
+++ Ticketsysteem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ticketsysteem.Startup))]
+namespace Ticketsysteem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
